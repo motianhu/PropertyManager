a2afe92aaaf6fb0ec4af2636ccfc07fb01d3b989
Moth <motianhu@qq.com>
2015-08-08 09:39:29 +0800

    5、横屏的时候图标显示有问题。右边空着，下边显示不全，而且没有滚动条。

--- src/com/smona/app/propertymanager/util/PropertyConstants.java
+++ src/com/smona/app/propertymanager/util/PropertyConstants.java
@@ -3,11 +3,11 @@ package com.smona.app.propertymanager.util;
 public class PropertyConstants {
     private static final String IP = "1.193.54.126";
     public static final String URL = "http://" + IP
-            + ":3715/AppCommDataHandler.ashx";
+            + ":3718/AppCommDataHandler.ashx";
 
     public static final String UPLOAD = "http://" + IP
-            + ":3715/upload/Default.aspx";
+            + ":3718/upload/Default.aspx";
 
     public static final String USERNAMRE = "18937113913";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "888888";
 }
